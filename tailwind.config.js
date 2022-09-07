d65111dfbb626724e6e0ceea386497232f8ace2f
Dinesh485 <ghantasaladinesh@gmail.com>
2022-09-07 21:42:41 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+       sm: '16px',
+       base: '18px',
+       lg: '24px',
+       xl: '28px',
+       '2xl': '36px',
+       '3xl': '54px',
+    },
+    extend: {
+      colors:{
+        primary : '#FFF80A',
+        secondary: '#FFF80A99',
+        muted: '#FFF80A33',
+        'custom-black': '#272727',
+        'custom-gray' : '#00000099'
+      },
+      fontFamily:{
+        poppins : ['Poppins', 'sans-serif']
+      },
+      fontWeight:{
+        semibold: 600
+      }
+    },
   },
   plugins: [],
 }
