ea7d8bf5f02a67b51437ac4d94f945bbc1c32531
Dinesh485 <ghantasaladinesh@gmail.com>
2022-09-08 19:55:07 +0530

    projectCard and blogPostCard added

---
+++ components/blogPostCard/blogPostCard.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./blogPostCard.module.css";
+const BlogPostCard = ({image, category, title, date}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.imageHolder}>
+        {image ? <Image src = {image} alt = 'blog image' layout = 'fill' /> : <div className={styles.noImage}>No Image</div>}
+      </div>
+      <div className={styles.contents}>
+          <span >{category}</span>
+          <h4>{title}</h4>
+          <p>{date}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPostCard;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import NewsLetter from 'components/newsLetter/newsLetter'
+
+
 import styles from 'styles/home.module.css'
 
+import BlogPostCard from 'components/blogPostCard/blogPostCard'
+import BlogImage from 'public/images/BlogImage1.png'
 export default function Home() {
   return (
     <>
-      <NewsLetter />
+      <BlogPostCard title = {'title this is title and for a test '} category = {'category'} date = {'date'} image = {BlogImage} />
     </>
   )
 }
