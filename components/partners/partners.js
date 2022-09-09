d7f5155a73d0fdc3f34263bba4957066bb60b2ec
Dinesh485 <ghantasaladinesh@gmail.com>
2022-09-09 11:48:55 +0530

    footer and partners components added

---
+++ components/partners/partners.js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import partener1 from 'public/images/partners/partner1.png'
+import partener2 from 'public/images/partners/partner2.png'
+import partener3 from 'public/images/partners/partner3.png'
+import partener4 from 'public/images/partners/partner4.png'
+import partener5 from 'public/images/partners/partner5.png'
+
+const Partners = () => {
+
+   const partners = [
+    { name: 'name',
+      image: partener1,
+    },
+    { name: 'name',
+      image: partener2,
+    },
+    { name: 'name',
+      image: partener3,
+    },
+    { name: 'name',
+      image: partener4,
+    },
+    { name: 'name',
+      image: partener5,
+    },
+   
+   ]
+
+    return ( 
+        <div className="flex flex-wrap items-center justify-between gap-6">
+             {partners.map((item, index )=>{
+                return(
+                    <div className = 'w-[100px] lg:w-[140px] xl:w-[180px]' key = {index}>
+                        <Image src = {item.image} alt = {item.name}/>
+                    </div>
+                )
+             })}
+        </div>
+     );
+}
+ 
+export default Partners;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 
 
+import Partners from 'components/partners/partners'
 import styles from 'styles/home.module.css'
 
-import BlogPostCard from 'components/blogPostCard/blogPostCard'
-import BlogImage from 'public/images/BlogImage1.png'
+
 export default function Home() {
   return (
     <>
-      <BlogPostCard title = {'title this is title and for a test '} category = {'category'} date = {'date'} image = {BlogImage} />
+     
     </>
   )
 }
