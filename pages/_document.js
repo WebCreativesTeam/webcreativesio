44b4dc1b7238bf89f868023f5a058271fdd4ade4
Dinesh485 <ghantasaladinesh@gmail.com>
2022-11-13 16:11:36 +0530

    lag on mobile fixed

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin ='true' />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet" />
         </Head>
-        <body >
+        <body  >
+          
           <Main />
           <NextScript />
         </body>
